690d10fd4d2b178f7496c1884cfa1e5924254242
Sergey Sokolov <sokexp@yandex.ru>
2023-12-01 02:27:14 +0300

    Правки

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
 const lightStyle = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=light]');
 const darkStyle = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]');
-const darkMedia = matchMedia('(prefers-color-scheme: dark)');
 const switcherButtons = document.querySelectorAll('.theme-switcher__button');
 
 function setupSwitcher() {
-  const savedScheme = getSavedScheme();
 
   [...switcherButtons].forEach((btn) =>{
     btn.addEventListener('click', () => {
@@ -45,57 +43,5 @@ function switchMedia(scheme) {
 
 }
 
-function getSavedScheme() {
-  return localStorage.getItem('color-scheme');
-}
 
 setupSwitcher();
-
-// const themeButtons = document.querySelectorAll('.header__theme-menu-button');
-//
-// themeButtons.forEach((button) => {
-//   button.addEventListener('click', () => {
-//     themeButtons.forEach((btn) => {
-//       btn.classList.remove('header__theme-menu-button_active');
-//       btn.removeAttribute('disabled');
-//     });
-//     if (
-//       [...button.classList].includes('header__theme-menu-button_type_light')
-//     ) {
-//       changeTheme('light');
-//     } else if (
-//       [...button.classList].includes('header__theme-menu-button_type_dark')
-//     ) {
-//       changeTheme('dark');
-//     } else {
-//       changeTheme('auto');
-//     }
-//     button.classList.add('header__theme-menu-button_active');
-//     button.setAttribute('disabled', true);
-//   });
-// });
-//
-// function changeTheme(theme) {
-//   document.body.className = 'page';
-//   document.body.classList.add(`theme_${theme}`);
-//   localStorage.setItem('theme', theme);
-// }
-//
-// function initTheme() {
-//   const theme = localStorage.getItem('theme');
-//   if (theme) {
-//     changeTheme(theme);
-//     themeButtons.forEach((btn) => {
-//       btn.classList.remove('header__theme-menu-button_active');
-//       btn.removeAttribute('disabled');
-//     });
-//     document
-//       .querySelector(`.header__theme-menu-button_type_${theme}`)
-//       .classList.add('header__theme-menu-button_active');
-//     document
-//       .querySelector(`.header__theme-menu-button_type_${theme}`)
-//       .setAttribute('disabled', true);
-//   }
-// }
-//
-// initTheme();
